21e1d28dc31c1ef9a4b62effaeabafc790b0793b
brian odinga <afriqenjh@gmail.com>
2023-02-16 16:21:13 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that print a line using printf function
- * Return: 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 
 int main(void)
 {
-prinf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
