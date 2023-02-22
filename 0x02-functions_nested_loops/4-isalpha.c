2dfcc1b46f926f4b4101f55065216a7a6271b64f
brian odinga <afriqenjh@gmail.com>
2023-02-22 03:08:49 -0800

    main

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,9 @@
 
 /**
  * _isalpha - Checks for alphabetic character.
- * written by brian
  * @c: character to check
- * return: 1 if character is a letter, 0 if not
+ * Return: 1 if character is a letter, otherwise 0
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
